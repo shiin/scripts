2275b257b503731860ebf62db1ea6bb88b686365
Shawn Lee <shiin.lee@AMDN1224.digital.local>
2018-01-04 11:00:18 +0100

    Update the setup script, 'ubuntu.sh'
    Remove redendant things.
    Add install command for bash-git-prompt and awesome_vimrc

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -3,9 +3,21 @@
 # Required version 16.01 or later.
 
 # Build essential
-#sudo apt install autoconf autopoint ctags cscope gitk
+sudo apt install autoconf autopoint ctags cscope gitk
 
 # Required building EFL library.
-#sudo apt install ccache libtool check libssl-dev libsystemd-dev libjpeg-dev libglib2.0-dev libgstreamer1.0-dev libluajit-5.1-dev libfreetype6-dev libfontconfig1-dev libfribidi-dev 
+sudo apt install ccache libtool check libssl-dev libsystemd-dev libjpeg-dev libglib2.0-dev libgstreamer1.0-dev libluajit-5.1-dev libfreetype6-dev libfontconfig1-dev libfribidi-dev
 
 sudo apt install libx11-dev libxcursor-dev libxrender-dev libxrandr-dev libxfixes-dev libxdamage-dev libxcomposite-dev libxss-dev libxpresent-dev libxext-dev libxinerama-dev libxkbfile-dev libxtst-dev libxcb1-dev libxcb-shape0-dev libxcb-keysyms1-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev libgif-dev libtiff5-dev libpoppler-cpp-dev libspectre-dev libraw-dev librsvg2-dev libcairo2-dev libudev-dev libblkid-dev libmount-dev libdbus-1-dev libpulse-dev libsndfile1-dev libbullet-dev libgstreamer-plugins-base1.0-dev
+
+# install bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+echo "GIT_PROMPT_ONLY_IN_REPO=1" >> ~/.bashrc
+echo "source ~/.bash-git-prompt/gitprompt.sh"
+
+# install awesome_vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Reactive bashrc
+source ~/.bashrc
